64b06c5b804d6b54626067d9b4355455003c0054
mJunaid15 <junaidjjd44@gmail.com>
2022-04-23 18:41:15 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+const App =()=>{
+  const [num , setNum]= useState();
+  const [move, setMove] =useState();
+  const [name, setName] =useState();
+
+
+  useEffect(()=>{
+    // alert('hi')
+    const link = `https://pokeapi.co/api/v2/pokemon/${num}`;
+    async function getData(){
+      const res = await axios.get(link);
+      console.log(res);
+      setName(res.data.name);
+      setMove(res.data.moves[2].move.name);
+
+    }
+    getData();
+
+  });
+
+  return(
+    <>
+    <h1>you choose <span style={{color:'red'}}>{num} </span> value </h1>
+    <h1>my name is <span style={{color:'red'}}>{name}</span>   </h1>
+    <h1>you choose <span style={{color:'red'}}>{move}</span>  moves </h1>
+
+    <select value={num} onChange={(event)=>{
+      setNum(event.target.value);
+
+    }}>
+      <option value="1">1</option>
+      <option value="25">25</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+
+
+    </select>
+    </>
   );
-}
-
-export default App;
+};
+ export default App;
